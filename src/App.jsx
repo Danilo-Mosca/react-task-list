0a5b8be838cca49e788992cc2018e0b95a4d4a0a
Danilo-Mosca <danilo.mosca85@gmail.com>
2024-12-11 15:47:52 +0100

    added function TasksNotCompleted for filtering "backlog” o “in_progress” tasks

--- src/App.jsx
+++ src/App.jsx
@@ -2,35 +2,23 @@
 import tasks from "./data/tasks";
 /* Importo i componenti: */
 import HeaderComponent from "./components/HeaderComponent";
-// import TasksCompleted from "./components/TasksCompleted";
+import TasksCompleted from "./components/TasksCompleted";
+import TasksNotCompleted from "./components/TasksNotCompleted";
 // import "./App.css";
 
 function App() {
   //parte logica
-  function tasksCompleted() {
-    let tasksCompleted = [];
-    tasksCompleted = tasks.filter((element) => {
-      return element.state.includes("completed")
-    }).map((element) => {
-      return <li key={element.id}>
-        <h3>{element.title}</h3>
-        <p>Priority: {element.priority}</p>
-        <p>Est. time {element.estimatedTime}</p>
-      </li>
-    });
-
-    return tasksCompleted;
-  }
 
   //parte html da ritornare
   return (
     //Prima richiamo il componente <React.Fragment> o nella forma abbreviata <> per permettere di tornare più elementi:
     <>
       <HeaderComponent />
-      <h1>Hello World!</h1>
+      <h2>Current Tasks ()</h2>
+      <ul><TasksNotCompleted /></ul>
 
-      <h2>Completed Tasks ({tasksCompleted().length})</h2>
-      <ul>{tasksCompleted()}</ul>
+      <h2>Completed Tasks ()</h2>
+      <ul><TasksCompleted /></ul>
     </>
   );
 }
